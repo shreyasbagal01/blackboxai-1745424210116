31ab13461f70000b6d4d8edb49cbaa7a7de2c8a7
Shreyas Bagal <112772990+shreyasbagal01@users.noreply.github.com>
2025-04-23 21:34:57 +0530

    Add e-vitaran/client/src/index.js 1745424296844

---
+++ e-vitaran/client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+
+// Add Razorpay script
+const addRazorpayScript = () => {
+  const script = document.createElement('script');
+  script.src = 'https://checkout.razorpay.com/v1/checkout.js';
+  script.async = true;
+  document.body.appendChild(script);
+};
+
+addRazorpayScript();
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
